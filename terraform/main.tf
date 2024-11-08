900b42d0d241fec84220d8276141dd691c4d52e0
Ravishekar <ravikshekar.kumar@wetransfer.com>
2024-11-08 23:43:01 +0100

    add_github_actions_24

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,6 +121,11 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 # Variables for script and dependency locations
 variable "bronze_script_location" {}
 variable "common_utils_location" {}
