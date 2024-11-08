a89e1e3a943f6384ba9481486be55efecc1a9f4f
Ravishekar <ravikshekar.kumar@wetransfer.com>
2024-11-08 23:33:13 +0100

    add_github_actions_22

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,29 +105,6 @@ resource "aws_glue_crawler" "bronze_crawler" {
   })
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_eventbridge_to_slack_role"
-
-  assume_role_policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
-      {
-        Action : "sts:AssumeRole",
-        Principal : { 
-          "Service" : "lambda.amazonaws.com" 
-        },
-        Effect : "Allow",
-        Sid : ""
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 # Variables for script and dependency locations
 variable "bronze_script_location" {}
 variable "common_utils_location" {}
