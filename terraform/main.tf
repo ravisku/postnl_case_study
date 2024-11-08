fee52bc21c3ff13dfe8b5d4bafa06574fe561c53
Ravishekar <ravikshekar.kumar@wetransfer.com>
2024-11-08 23:19:04 +0100

    add_github_actions_18

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,8 +109,8 @@ resource "aws_iam_role" "lambda_role" {
   name = "lambda_eventbridge_to_slack_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    Version : "2012-10-17",
+    Statement : [
       {
         Action : "sts:AssumeRole",
         Principal : { Service : "lambda.amazonaws.com" },
