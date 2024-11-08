c3e19149097a6e81c5bd5cd64844386ec6130b41
Ravishekar <ravikshekar.kumar@wetransfer.com>
2024-11-08 20:10:02 +0100

    add_github_actions_3

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,3 +65,46 @@ resource "aws_iam_role_policy_attachment" "glue_s3_policy_attach" {
   role       = aws_iam_role.glue_role.name
   policy_arn = aws_iam_policy.glue_s3_access_policy.arn
 }
+
+# Define Glue ETL Job for Bronze Layer
+resource "aws_glue_job" "bronze_job" {
+  name     = "bronze-events-job"
+  role_arn = aws_iam_role.glue_role.arn
+
+  command {
+    # GitHub Actions will provide the location of the script in S3
+    script_location = var.bronze_script_location
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--extra-py-files" = var.common_utils_location
+  }
+}
+
+
+resource "aws_glue_catalog_database" "bronze_database" {
+  name = "bronze"
+}
+
+resource "aws_glue_crawler" "bronze_crawler" {
+  name = "bronze_crawler"
+  role = aws_iam_role.glue_role.arn
+
+  database_name = aws_glue_catalog_database.bronze_database.name
+
+  s3_target {
+    path = "s3://postnl-case-study/target_files/bronze/events/"
+  }
+
+  configuration = jsonencode({
+    "Version" : 1.0,
+    "Grouping" : {
+      "TableGroupingPolicy" : "CombineCompatibleSchemas"
+    }
+  })
+}
+
+# Variables for script and dependency locations
+variable "bronze_script_location" {}
+variable "common_utils_location" {}
